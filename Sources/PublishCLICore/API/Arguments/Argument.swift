5f828a4485d9a49547a9a80f5ed8f137551ddc0c
Dorian Grolaux <6209874+MrSkwiggs@users.noreply.github.com>
2020-01-05 15:22:25 +0100

    🚧 Add new Arguments enum

---
+++ Sources/PublishCLICore/API/Arguments/Argument.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dorian Grolaux on 05/01/2020.
+//
+
+import Foundation
+
+enum Argument {
+    case new
+    case generate
+    case run
+    case deploy
+}
