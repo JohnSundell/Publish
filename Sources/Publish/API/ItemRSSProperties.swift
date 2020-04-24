e36e99578b896cfc66d28486fe4b79969f806d77
John Sundell <john@sundell.co>
2020-04-24 14:12:17 +0200

    RSS: Add APIs for defining a prefix and suffix for an item’s body
    This change makes it possible to define a prefix and/or suffix that will
    get added to an item’s body when rendered within an RSS feed. This can,
    for example, be used to provide specific intro paragraphs for RSS items,
    to embed media players, and so on.
    
    Just like the equivalent APIs for titles, these prefixes/suffixes are
    rendered as-is and are not converted to HTML automatically.

--- Sources/Publish/API/ItemRSSProperties.swift
+++ Sources/Publish/API/ItemRSSProperties.swift
@@ -16,15 +16,26 @@ public struct ItemRSSProperties: Codable, Hashable {
     public var titlePrefix: String?
     /// Any suffix that should be added to the item's title within an RSS feed.
     public var titleSuffix: String?
+    /// Any prefix that should be added to the item's body HTML within an RSS feed.
+    public var bodyPrefix: String?
+    /// Any suffix that should be added to the item's body HTML within an RSS feed.
+    public var bodySuffix: String?
 
     /// Initialize an instance of this type
     /// - parameter guid: Any specific GUID that should be added for the item.
     /// - parameter titlePrefix: Any prefix that should be added to the item's title.
     /// - parameter titleSuffix: Any suffix that should be added to the item's title.
+    /// - parameter bodyPrefix: Any prefix that should be added to the item's body HTML.
+    /// - parameter bodySuffix: Any suffix that should be added to the item's body HTML.
     public init(guid: String? = nil,
                 titlePrefix: String? = nil,
-                titleSuffix: String? = nil) {
+                titleSuffix: String? = nil,
+                bodyPrefix: String? = nil,
+                bodySuffix: String? = nil) {
         self.guid = guid
         self.titlePrefix = titlePrefix
+        self.titleSuffix = titleSuffix
+        self.bodyPrefix = bodyPrefix
+        self.bodySuffix = bodySuffix
     }
 }
