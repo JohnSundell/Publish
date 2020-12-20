a5edc101fdbdb8b6f346347bfac956814563a624
Tanishq Kancharla <tklightningfast@gmail.com>
2020-12-20 15:42:02 -0500

    Removed redundant code in WebsiteRunner

--- Sources/PublishCLICore/WebsiteRunner.swift
+++ Sources/PublishCLICore/WebsiteRunner.swift
@@ -15,77 +15,8 @@ internal struct WebsiteRunner {
     func run() throws {
         let generator = WebsiteGenerator(folder: folder)
         try generator.generate()
-
-        let outputFolder = try resolveOutputFolder()
-
-        let serverQueue = DispatchQueue(label: "Publish.WebServer")
-        let serverProcess = Process()
-
-        print("""
-        🌍 Starting web server at http://localhost:\(portNumber)
-
-        Press ENTER to stop the server and exit
-        """)
-
-        serverQueue.async {
-            do {
-                _ = try shellOut(
-                    to: "python -m \(self.resolvePythonHTTPServerCommand()) \(self.portNumber)",
-                    at: outputFolder.path,
-                    process: serverProcess
-                )
-            } catch let error as ShellOutError {
-                self.outputServerErrorMessage(error.message)
-            } catch {
-                self.outputServerErrorMessage(error.localizedDescription)
-            }
-
-            serverProcess.terminate()
-            exit(1)
-        }
-
-        _ = readLine()
-        serverProcess.terminate()
-    }
-}
-
-private extension WebsiteRunner {
-    func resolveOutputFolder() throws -> Folder {
-        do { return try folder.subfolder(named: "Output") }
-        catch { throw CLIError.outputFolderNotFound }
-    }
-
-    func resolvePythonHTTPServerCommand() -> String {
-        if resolveSystemPythonMajorVersionNumber() >= 3 {
-            return "http.server"
-        } else {
-            return "SimpleHTTPServer"
-        }
-    }
-
-    func resolveSystemPythonMajorVersionNumber() -> Int {
-        // Expected output: `Python X.X.X`
-        let pythonVersionString = try? shellOut(to: "python --version")
-        let fullVersionNumber = pythonVersionString?.split(separator: " ").last
-        let majorVersionNumber = fullVersionNumber?.first
-        return majorVersionNumber?.wholeNumberValue ?? 2
-    }
-
-    func outputServerErrorMessage(_ message: String) {
-        var message = message
-
-        if message.hasPrefix("Traceback"),
-           message.contains("Address already in use") {
-            message = """
-            A localhost server is already running on port number \(portNumber).
-            - Perhaps another 'publish run' session is running?
-            - Publish uses Python's simple HTTP server, so to find any
-              running processes, you can use either Activity Monitor
-              or the 'ps' command and search for 'python'. You can then
-              terminate any previous process in order to start a new one.
-            """
-        }
-
-        fputs("\n❌ Failed to start local web server:\n\(message)\n", stderr)
+        
+        let server = WebsiteServer.init(folder: folder, portNumber: portNumber)
+        try server.serve()
     }
 }
