ff06e265f9007934d9f2878ae2dde254f413cd77
Omar Albeik <omaralbeik@gmail.com>
2021-05-25 01:10:45 +0200

    Fix typos

--- Sources/Publish/API/PlotEnvironmentKeys.swift
+++ Sources/Publish/API/PlotEnvironmentKeys.swift
@@ -1,3 +1,9 @@
+/**
+*  Publish
+*  Copyright (c) John Sundell 2019
+*  MIT license, see LICENSE file for details
+*/
+
 import Plot
 import Ink
 
--- Sources/PublishCLICore/ProjectKind.swift
+++ Sources/PublishCLICore/ProjectKind.swift
@@ -1,3 +1,9 @@
+/**
+*  Publish
+*  Copyright (c) John Sundell 2019
+*  MIT license, see LICENSE file for details
+*/
+
 import Foundation
 
 internal enum ProjectKind: String {
