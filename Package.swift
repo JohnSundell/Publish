f4fabe5af3b03110404e98a0ec58946ad4ee0cff
Vithanco <klaus@vithanco.com>
2023-08-10 21:42:24 +0200

    Update Package.swift
    changed Plot dependency version to 0.14.0

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .package(
             name: "Plot",
             url: "https://github.com/johnsundell/plot.git",
-            from: "0.9.0"
+            from: "0.14.0"
         ),
         .package(
             name: "Files",
